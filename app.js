f352d7d12abdda6a26ad17ad221ee0f80715507f
fahadi04 <fahadmasrur786@gmail.com>
2024-09-01 23:16:34 +0530

    just commit

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ var Typed = new Typed('.auto-text', {
   BackSpeed: 150,
   loop: true
 });
-
 // -------------------------------------------------------
-
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
-
 function opentab(tabname) {
   for (tablink of tablinks) {
     tablink.classList.remove("active-link");
@@ -21,15 +18,11 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
-
 // ==========================Side Bar===================
-
 var sidemenu = document.getElementById("sidemenu");
-
 function openmenu() {
   sidemenu.style.right = "0";
 }
-
 function closemenu() {
   sidemenu.style.right = "-200px";
 }
